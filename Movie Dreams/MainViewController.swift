280e169c0daac466afc2f2340b721dc4f91004b9
Evgeniy Kotovich <sxekot@gmail.com>
2022-05-24 19:57:00 +0300

    add movie card and rating stars

--- Movie Dreams/MainViewController.swift
+++ Movie Dreams/MainViewController.swift
@@ -16,12 +16,22 @@ class MainViewController: UIViewController {
     
     let button: UIButton = {
         $0.setTitle("Button", for: .normal)
+        $0.addTarget(nil, action: #selector(buttonTapped), for: .touchUpInside)
+        $0.translatesAutoresizingMaskIntoConstraints = false
         return $0
     }(UIButton())
+    
+    @objc func buttonTapped(_ sender: UIButton) {
+        present(MovieCardController(), animated: true, completion: nil)
+    }
 
     func initialize() {
         view.backgroundColor = .green
+        view.addSubview(button)
+        button.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     }
 
+    
 }
 
