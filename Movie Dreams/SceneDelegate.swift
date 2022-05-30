076d209c5a617564c2516dfc6ce997590eb0e9d2
ckost9n <ckost9n@gmail.com>
2022-05-30 14:41:13 +0300

    add tabbar

--- Movie Dreams/SceneDelegate.swift
+++ Movie Dreams/SceneDelegate.swift
@@ -15,9 +15,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let controller = MainViewController()
-        let navController = UINavigationController(rootViewController: controller)
-        window?.rootViewController = navController
+        let controller = MainTabBarController()
+//        let navController = UINavigationController(rootViewController: controller)
+        window?.rootViewController = controller
         window?.makeKeyAndVisible()
         window?.overrideUserInterfaceStyle = .light
     }
