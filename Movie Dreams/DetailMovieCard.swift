08a7abeebaf8c06243c19ffc185082fb524609a0
Ilia Shapovalov <greateman@mail.ru>
2022-06-03 21:37:39 +0300

    Add DetailMovieData model for decode JSON. Add DetailMovieCard to represent information for MovieCardController. Add method to configure movieTitle, rewievLabel and movieSubTitle

---
+++ Movie Dreams/DetailMovieCard.swift
@@ -0,0 +1,8 @@
+//
+//  DetailMovieCard.swift
+//  Movie Dreams
+//
+//  Created by Илья Шаповалов on 03.06.2022.
+//
+
+import Foundation
---
+++ Movie Dreams/DetailMovieData.swift
@@ -0,0 +1,8 @@
+//
+//  DetailMovieData.swift
+//  Movie Dreams
+//
+//  Created by Илья Шаповалов on 03.06.2022.
+//
+
+import Foundation
